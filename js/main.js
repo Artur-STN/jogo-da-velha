eaae84bc4a698dcf7cbedd7dad9c3cd3541cbb50
Artur Santos <artur_teixeira10@hotmail.com>
2021-11-18 22:16:33 -0300

    code

--- js/main.js
+++ js/main.js
@@ -1,32 +1,72 @@
-var playerUm = ["playerUm", "X"]
-var playerDois = ["playerDois", "O"]
+var jogadorUm;
+var jogadorDois;
 
-var first;
+var vez;
 
-function primeiroJogador() {
-    
-    let numeroAleatorio = Math.floor(Math.random() * 2) + 1
+function closeMenu(opçãoEscolhida) {
 
-    if (numeroAleatorio == 1) {
+    let menu = document.getElementById("menu")
 
-        first = "player Um"
+    menu.style.left = "-100vw"
 
-    } else if (numeroAleatorio == 2) {
+    jogadorUm = opçãoEscolhida
 
-        first = "player Dois"
+    if (jogadorUm == "X") {
+
+        jogadorDois = "O"
+        vez = "jogadorUm"
+
+    } else if(jogadorUm == "O"){
+
+        jogadorDois = "X"
+        vez = "jogadorDois"
 
     }
 
-    alert(`Primeiro a jogar é ${first}`)
-
 }
-
-primeiroJogador()
 
 function insertLetter(divId) {
 
     let div = document.getElementById(divId)
 
+    if (vez == "jogadorUm") {
+
+        div.innerText = "X"
+        vez = "jogadorDois"
+
+        div.style.background = "#43AA8B"
+
+    } else if(vez == "jogadorDois") {
+
+        div.innerText = "O"
+        vez = "jogadorUm"
+
+        div.style.background = "#F94144"
+
+    }
+
+    div.style.color = "white"
+    div.style.border = "1px solid white"
+    div.style.borderRadius = "10px"
+
+}
+
+function newGame() {
     
     
+    for (let index = 1; index <= 9; index++) {
+        
+        let div = document.getElementById(`divNumber${index}`)
+
+        div.innerText = ""
+        div.style.background = "white"
+        div.style.border = "1px solid black"
+        div.style.borderRadius = "0"
+        
+    }
+
+    let menu = document.getElementById("menu")
+
+    // menu.style.left = "0"
+
 }
